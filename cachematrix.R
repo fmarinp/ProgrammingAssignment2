a2c7d18d02d56942eb61fde3f679241979469dd2
Felipe Marin - Laptop <fmarinp05@gmail.com>
2015-03-23 11:03:57 +1100

    first attempt

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,42 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#steps needed:
+#1.  set the value of the vector
+#2.  get the value of the vector
+#3.  set the value of the mean
+#4.  get the value of the mean
 
+
+#just copy pasting now from makevector.. mean-->solve
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinvm <- function(solve) m <<- solve
+    getinvm <- function() m
+    list(set=set, get=get,
+         setinvm = setinvm,
+         getinvm = getinvm)    
 }
 
 
 ## Write a short comment describing this function
+#works as intended.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvm()
+    if(!is.null(m)){
+        message("getting cached data..")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data,...)
+    x$setinvm(m)
+    m
 }
+
